8379a87c926ba1fa32d63cd49e81a91bcb9595f8
Ranjithkumar <109387685+ranjithrk24@users.noreply.github.com>
2022-07-31 23:22:11 +0530

    Add files via upload

---
+++ reverse.py
@@ -0,0 +1,13 @@
+def reverse():
+    string=input()
+    reversed_string=''
+    for i in string:
+        reversed_string=i+reversed_string
+    return reversed_string
+print(reverse())
+
+
+
+    
+
+
